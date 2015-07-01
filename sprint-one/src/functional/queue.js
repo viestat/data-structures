ff79181756bb41e66cb3a68c8bcf21b5b6d1493e
viestat <viestat@icloud.com>
2015-07-01 14:55:31 -0700

    Complted stack and queue in functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,30 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var length = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[length] = value;
+    length++;
   };
 
   someInstance.dequeue = function(){
+    var result;
+    result = storage[0];
+    if (length > 0) {
+      length--;
+    }
+    //fill in the hole
+    for(var i = 0; i < length; i++)
+    {
+      storage[i] = storage[i + 1];
+    }
+    return result;
   };
 
   someInstance.size = function(){
+    return length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,26 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[length] = value;
+    length++;
   };
 
   someInstance.pop = function(){
+    var returnValue;
+    if (length > 0)
+    {
+      returnValue = storage[length - 1];
+      length--;
+    }
+    return returnValue;
   };
 
   someInstance.size = function(){
+    return length;
   };
 
   return someInstance;
