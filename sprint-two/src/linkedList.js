fec8585d5cd42b1fac86fdeed4bab0d9bd050bb2
viestat <viestat@icloud.com>
2015-07-02 11:36:00 -0700

    Completed linkedList and tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    if(list.head === null)
+    {
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+    }
+      list.tail = newNode;
+
   };
 
   list.removeHead = function(){
+    var result = list.head.value;
+    list.head = list.head.next; 
+    return result;
   };
 
   list.contains = function(target){
+
+    var checkNode = function(node) {
+      if (node.value === target) {
+        return true;
+      }
+      if (node.next === null)
+      {
+        return false;
+      }
+      return checkNode(node.next);
+    };
+
+    return checkNode(list.head);
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,10 +1,14 @@
+
+
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
+  if (newTree.value === undefined) {
+  	//debugger;
+  }
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -15,14 +19,40 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+	this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
 
+	//var found = false;
+	var checkNode = function(node) {
+		if(node.value === target)
+		{
+			return true;
+		} else {
+			for(var i = 0; i < node.children.length; i++)
+			{
+				if(checkNode(node.children[i]))
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	//checkNode(this);
+	//return found;
+	//debugger;
+	return checkNode(this);
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+//Library code
+function extend(to, from) {
+	for (var key in from) {
+		to[key] = from[key];
+	}
+}
